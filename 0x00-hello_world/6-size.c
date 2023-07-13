a216244d3b2a79529d0d230099a58f40037ed5ca
Peter Precious <peterprecious370@gmail.com>
2023-07-13 12:36:38 -0700

    fixed sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("the size of a character is: %lu.\n", (unsigned long)sizeof(c));
-	printf("the size of an integer is: %lu.\n", (unsigned long)sizeof(i));
-	printf("the size of a long integer is: %lu.\n", (unsigned long)sizeof(li));
-	printf("the size of a long long integer is: %lu.\n", (unsigned long)sizeof(lli));
-	printf("the size of a float is: %lu.\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu  byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
