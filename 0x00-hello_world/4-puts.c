fd9b9a5a6938eef199368b15f74ff586f7d1fea8
Peter Precious <peterprecious370@gmail.com>
2023-07-13 11:19:05 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this the main function
+ *
+ * Return: 0 after execution
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
