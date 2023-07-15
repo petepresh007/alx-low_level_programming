c8fa8e0cf9cc7f0214eba0e9edf8cfc39ed77ef2
peter precious <peterprecious370@gmail.com>
2023-07-15 03:02:03 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function to print digits in base ten
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+}
