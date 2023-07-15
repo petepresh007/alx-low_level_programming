00c61631c9a61742626f548d6b29e3e035c7dd99
peter precious <peterprecious370@gmail.com>
2023-07-15 02:14:38 -0700

    modified

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - function to print lower followed by upper
+ * main - a program that prints alphabet in lower followed by upper
  * Return: 0 (success)
  */
 int main(void)
 {
 	char lo_up[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i = 0;
+	int i;
 
 	for (i = 0; i <= 52; i++)
 		putchar(lo_up[i]);
 	putchar('\n');
-	return (0);
 }
