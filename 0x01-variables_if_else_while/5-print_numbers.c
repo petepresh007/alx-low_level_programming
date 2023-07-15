6193a112f9bab7ba9bd6adbae84268e0c6a65e75
peter precious <peterprecious370@gmail.com>
2023-07-15 02:54:33 -0700

    add numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function for numbers in base ten
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
