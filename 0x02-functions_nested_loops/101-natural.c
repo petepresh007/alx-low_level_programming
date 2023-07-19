f36d83e11eefb5246be311f16bf979c53f0f590c
Peter Precious <peterprecious370@gmail.com>
2023-07-19 04:49:21 -0700

    program

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program to print multiple of 3 or 5
+ * Return: 0 {success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
