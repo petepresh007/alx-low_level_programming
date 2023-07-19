0e712dde358e5208dc37433485f0e27098465571
peter precious <peterprecious370@gmail.com>
2023-07-19 04:30:16 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function to add two numbers
+ * @i: value one
+ * @j: value two
+ * Return: sum of two numbers
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
