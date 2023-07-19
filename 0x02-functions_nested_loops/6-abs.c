01d43a1f76aa4ffb8503b1f5b5d8302ab7fb85fe
peter precious <peterprecious370@gmail.com>
2023-07-19 03:30:42 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function to print absolute value
+ * @i: check for the absolute value
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
