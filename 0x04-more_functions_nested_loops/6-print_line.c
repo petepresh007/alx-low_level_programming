7c1cde7201d966a9fa5367dc3dd12659651bb641
Peter Precious <peterprecious370@gmail.com>
2023-07-20 10:04:37 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - program that prints a straight line
+ * @n: input from the users
+ * Return: straight line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
