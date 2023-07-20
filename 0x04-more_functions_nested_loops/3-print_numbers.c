e0c4885f6826102b1b35d48d2ccc49abd3186b8a
Peter Precious <peterprecious370@gmail.com>
2023-07-20 08:28:40 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - program that prints numbers
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
