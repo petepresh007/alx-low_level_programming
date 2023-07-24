948289cc8a0bde73462b051927f7032f1c423cd3
Peter Precious <peterprecious370@gmail.com>
2023-07-24 14:36:52 -0700

    arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - program that prints an array
+ * @a: arr
+ * @n: number of input
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
+
+}
