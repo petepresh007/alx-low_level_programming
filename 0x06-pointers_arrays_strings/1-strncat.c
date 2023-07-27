ae6af14e7b4992f1ffc2d02b14371e9fd5dde2b3
Peter Precious <peterprecious370@gmail.com>
2023-07-27 04:10:02 -0700

    concatenating values

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - a program to concatinate strings
+ * @dest: input
+ * @src: input
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
