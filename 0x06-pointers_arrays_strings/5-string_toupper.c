72e615710d1f6ab6e4b2d748c70b6f6f84087df5
Peter Precious <peterprecious370@gmail.com>
2023-07-27 07:00:55 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - program that changes a string to upper case
+ * @str: string to be inputted
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
