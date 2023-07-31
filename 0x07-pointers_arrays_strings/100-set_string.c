ff0fead76bcf4168ea962d8870f2ea4e15e49762
Peter Precious <peterprecious370@gmail.com>
2023-07-31 13:14:14 -0700

    pointing to a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set the pointer to a character
+ * @s: pointer
+ * @to: character
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
