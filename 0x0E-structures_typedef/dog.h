f2b854887afcbb3ffc1649f3afb0507e44489bd0
Peter Precious <peterprecious370@gmail.com>
2023-08-14 12:28:44 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dogs information
+ * @name: dogs name
+ * @age: dogs age
+ * @owner:dogs owner
+ * Description: No description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
