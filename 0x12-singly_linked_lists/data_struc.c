18e31248715d9d8a3cdc9213cbdbd9c51d14d37f
Peter Precious <peterprecious370@gmail.com>
2023-08-24 16:12:54 -0700

    print

---
+++ 0x12-singly_linked_lists/data_struc.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
