dd53b1d9e4ff714855c764e41baae4fdafd2daf8
Peter Precious <peterprecious370@gmail.com>
2023-08-29 07:17:59 -0700

    len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * listint_len - a function that prints the len
+ * @h: content of node
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *another_node = h;
+	size_t num_element = 0;
+
+	while (another_node != NULL)
+	{
+		num_element += 1;
+		another_node = another_node->next;
+	}
+	return (num_element);
+}
