0885dcb89b805570c59ad88019e1c4febbd5e476
Peter Precious <peterprecious370@gmail.com>
2023-08-29 08:53:05 -0700

    nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == o)
+	if (idx == 0)
 	{
 		new_node->next = temp;
 		*head = new_node;
