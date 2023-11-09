9889116b6ae5ecca3617f98b987fae96f1fd4e49
peter precious <peterprecious370@gmail.com>
2023-11-09 08:34:05 -0800

    dlist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all el
+ * @h: head
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int c;
+
+	c = 0;
+
+	if (h == NULL)
+		return (c);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
