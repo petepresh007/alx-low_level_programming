1bde054dea30e1282fe02d8d6c44c0148b8826cb
peter precious <peterprecious370@gmail.com>
2023-11-09 08:41:36 -0800

    len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the num
+ * @h: head of the list
+ * Return: the num of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int c;
+
+	c = 0;
+
+	if (h == NULL)
+		return (c);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
